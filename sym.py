08fc40c5caf08f3bbc2d5d91a8ef2cca999278b1
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-07-18 02:24:40 +0100

    Cleanup

--- sym.py
+++ sym.py
@@ -3,6 +3,10 @@ from collections import Counter
 from tabulate import tabulate
 from tqdm import tqdm
 
+# uses a board centred on origin to trivialise symmetry
+# N=6, indices = [-3,-2,-1,  1,2,3]
+# N=7, indices = [-3,-2,-1,0,1,2,3]
+
 def main():
   MAX_N = 100 # How big the board we should search up to
   k = 2 # how many Queens to place / branch on
@@ -14,9 +18,6 @@ def main():
     T = set() # Counter() for dict-derived multiset
     sumorbit = 0
     
-    # board centred on origin for trivial symmetry generation
-    # N=6, indices = [-3,-2,-1,  1,2,3]
-    # N=7, indices = [-3,-2,-1,0,1,2,3]
     indices = [i for i in range(-(N//2), N//2+1) if i != 0 or odd_N]
     midrc = [0] if odd_N else [1,-1] #  +  # cross/plus shaped "middle rows and columns"
     edges = [indices[0],indices[-1]] # [ ] # like +/evens in that 4 corners have 4-orbit like 2x2 centroid
