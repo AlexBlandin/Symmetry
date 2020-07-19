84ee562638a1182a8a7fa68adc4643651c8e5756
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-07-19 03:05:44 +0100

    Improved in prep for Q-aware branching/preplacement

--- sym.py
+++ sym.py
@@ -4,23 +4,23 @@ from tabulate import tabulate
 from tqdm import tqdm
 
 def main():
-  global k
+  global N, k
   MAX_N = 50 # How big the board we should search up to
-  k = 2 # how many Queens to pre-place / branch on, Q27 used 2 max and we only need 2 max, higher gets improved quotients but the combinatoric increase in combinations of points is far too much
+  k = 2 # how many Queens to pre-place / branch on, we focus on k=2 (same as Q27)
   
   table = [["N", "symmetries", "branches", "quotient"]]
   for _N in tqdm(range(1,MAX_N+1), ascii=True):
-    global N
     N, odd_N = _N, _N%2 # for N*N Board
     S, sum_S = set(), 0 # use multiset() for dict-derived multiset
     
     indices = [i for i in range(-(N//2), N//2+1) if i != 0 or odd_N]
-    midrc = [0] if odd_N else [1,-1] #  +  # cross/plus shaped "middle rows and columns"
-    edges = [indices[0],indices[-1]] # [ ] # like +/evens in that 4 corners have 4-orbit like 2x2 centroid
-    coron = lambda L: indices[:L] + indices[-L:] # coronal part of the board, edges == coron(1), Q27 used L=2
-    # in terms of symmetry, [] matches +/evens in terms of having two columns and two rows, in which each maps around in 8-orbits, and the 4*1 corners match the 2x2 centroid in their symmetries, so ends up equivalent
+    midrc = (0,) if odd_N else (1,-1) #  +  # cross/plus shaped "middle rows and columns"
+    edges = (indices[0], indices[-1]) # [ ] # like +/evens in that 4 corners have 4-orbit like 2x2 centroid
+    rings = lambda r: (*indices[:r],*indices[-r:]) # [O] # r outermost rings, rings(1)=edges, Q27 had r=2
+    # edges ~ even N midrc with 2 columns & 2 rows, each point with 8-orbits except edges' 4*1 corners and midrc's 2x2 centroid
     
-    region = coron(1) # midrc | edges | coron(2)
+    region = rings(1) # midrc | edges | rings(2)
+    len_s=0
     for points in preplacement(region, indices):
       if len(points) == k:
         syms = sym(points)
@@ -36,14 +36,10 @@ def preplacement(region, indices): # todo: preplacement/branching that is always
   return combinations(s, k)
 
 def sym(points): # from one set generate the symmetries as a set of frozen sets
-  rx = frozenset((-x,y) for x,y in points)
-  ry = frozenset((x,-y) for x,y in points)
-  rd = frozenset((y,x) for x,y in points)
-  ra = frozenset((-x,-y) for x,y in points)
-  r1 = frozenset((-y,x) for x,y in points)
-  r2 = frozenset((-y,-x) for x,y in points)
-  r3 = frozenset((y,-x) for x,y in points)
-  r4 = frozenset((x,y) for x,y in points) # r4 = identity
+  rx = frozenset((-x,y) for x,y in points); ry = frozenset((x,-y) for x,y in points)
+  rd = frozenset((y,x) for x,y in points); ra = frozenset((-x,-y) for x,y in points)
+  r1 = frozenset((-y,x) for x,y in points); r2 = frozenset((-y,-x) for x,y in points)
+  r3 = frozenset((y,-x) for x,y in points); r4 = frozenset(points) # 360° is identity
   return {rx,ry,rd,ra,r1,r2,r3,r4}
 
 def board(s):
