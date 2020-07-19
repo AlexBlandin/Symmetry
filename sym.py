2abae4da8460633ff394a13f06ebfa76e34dc784
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-07-19 03:16:16 +0100

    Improved in prep for Q-aware branching/preplacement

--- sym.py
+++ sym.py
@@ -4,10 +4,8 @@ from tabulate import tabulate
 from tqdm import tqdm
 
 def main():
-  global N, k
-  MAX_N = 50 # How big the board we should search up to
   k = 2 # how many Queens to pre-place / branch on, we focus on k=2 (same as Q27)
-  
+  MAX_N = 50 # how big the board we should search up to
   table = [["N", "symmetries", "branches", "quotient"]]
   for _N in tqdm(range(1,MAX_N+1), ascii=True):
     N, odd_N = _N, _N%2 # for N*N Board
@@ -20,7 +18,7 @@ def main():
     # edges ~ even N midrc: each point has 8-orbit except edges' 4*1 corners and midrc's 2x2 centroid
     
     region = rings(1) # midrc | edges | rings(2)
-    for points in preplacement(region, indices):
+    for points in preplacement(region, indices, k):
       if len(points) == k:
         syms = sym(points)
         S |= syms # S.update(syms) # swap out until 3.9 for multiset
@@ -30,7 +28,7 @@ def main():
     table.append([N, sum_S, len_S, quotient])
   open("./data/test.txt", mode="w").write(tabulate(table, headers="firstrow", floatfmt=["d","d","d",".8f"]))
 
-def preplacement(region, indices): # todo: preplacement/branching that is always Queens-legal
+def preplacement(region, indices, k): # todo: preplacement/branching that is always Queens-legal
   s = set(chain(product(indices, region), product(region, indices)))
   return combinations(s, k)
 
@@ -41,7 +39,7 @@ def sym(points): # from one set generate the symmetries as a set of frozen sets
   r3 = frozenset((y,-x) for x,y in points); r4 = frozenset(points)
   return {rx,ry,rd,ra,r1,r2,r3,r4}
 
-def board(s):
+def board(s, N):
   b, c2i = [[0]*N for _ in range(N)], lambda x: (x-1 if N%2==0 and x >= 1 else x) + N//2
   for x,y in s: b[c2i(x)][c2i(y)]=1
   return "\n".join("".join(map(str,b[i])) for i in range(N))
