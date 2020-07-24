c90bf1fdb153b5c2f9f66be0d37d31dd439f785a
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-07-24 23:23:54 +0100

    Additional results and speedup when known branches=0

--- sym.py
+++ sym.py
@@ -2,17 +2,21 @@ from itertools import chain, product, combinations
 from collections import Counter as multiset
 from tabulate import tabulate
 from psutil import Process
+from time import sleep
 from tqdm import tqdm
 from sys import argv
+import gc
 
 def main():
   STRATEGY = argv[1] if len(argv)>1 else "midrc" # midrc | rings1 | rings2 | rings3
   r = 1 if STRATEGY not in ["rings2","rings3"] else {"rings2":2,"rings3":3}[STRATEGY]
   for k in range(2,4*r+1): # how many Queens to pre-place / branch on
     try:
-      MAX_N = 50 if k==2 or STRATEGY in ["midrc","rings1"] else 30
+      MAX_N = 50 if k==2 or STRATEGY in ["midrc","rings1"] else 15 if k > 7 else 30
       table = [["N", "symmetries", "branches", "quotient", "orbits"],[0,0,0,0.0,{}]]
       for N in tqdm(range(1,MAX_N+1), ascii=True): # we could start at 3
+        if N < k:
+          table.append([N, 0, 0, 0.0, {}]); continue
         S, sum_S, odd_N = multiset(), 0, N%2
         indices = tuple(i for i in range(-(N//2), N//2+1) if i != 0 or odd_N)
         midrc = (0,) if odd_N else (1,-1)    #  +  # middle row/col
@@ -22,13 +26,13 @@ def main():
           if legal(points): # bypass for lawless
             syms = sym(points) # todo: may need to consider 'internal' orbits from derived/continued board-states/placements for completion (separate multiset and stats)
             S.update(syms); sum_S += len(syms)
-        rss = Process().memory_info().rss # rings2 k=6 N=14 uses 15.0 GB
+        rss = Process().memory_info().rss # rings2 k=6 N=14 uses 15GB, rings3 k=7 N=10 uses GB and N=11 uses over 28GB
         branches = len(S)
         quotient = sum_S/branches if branches else 0
         orbits = multiset(S.values())
         table.append([N, sum_S, branches, quotient, dict(sorted(orbits.items(), key=lambda o:o[0], reverse=True))])
-        if rss >= 10_000_000_000: # exit out if we're using too much memory (currently 10GB)
-          print(f"Exitting to avoid OOM, used {rss/1_000_000_000:.2f}GB")
+        if rss >= 10_000_000_000: # exit out if we're using too much memory (currently set at 10GB)
+          del S; sleep(5); gc.collect(); sleep(5); print(f"Ending k = {k}, N = {N} to avoid OOM, measured {rss/1_000_000_000:.2f}GB")
           break
     except KeyboardInterrupt: pass
     open(f"./data/{STRATEGY}.k{k}.txt", mode="w").write(tabulate(table, headers="firstrow", floatfmt=["d","d","d",".8f"]))
