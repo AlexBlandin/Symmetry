90d8cf2b23d8fd9b6f214989a56e15352935457f
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-07-29 13:14:55 +0100

    Now tracking the fundamentals directly in set F

--- sym.py
+++ sym.py
@@ -6,21 +6,24 @@ def main():
   k, MAX_N = 2, 50
   table = [["N", "branches", "reduced", "quotient", "orbits", "fundamental"]]
   for N in range(MAX_N+1):
-    S, odd_N = multiset(), N%2
+    B, F, odd_N = multiset(), set(), N%2
     midrc = (0,) if odd_N else (1,-1) # middle rows/cols
     indices = tuple(i for i in range(-(N//2), N//2+1) if i != 0 or odd_N)
-    def offset(x): return (x-1 if N%2==0 and x >= 1 else x) + N//2 # reverse the conversion from 0..N to -N//2..N//2
+    def offset(x): return (x-1 if N%2==0 and x >= 1 else x) + N//2 # reverse the conversion from 0..N-1 to -N//2..N//2
     def board(squares): return "\n".join("".join("#" if (x,y) in squares else "-" for x in indices) for y in indices)
     def legal(branch):
       (x,y), (a,b) = branch
-      ox,oy,oa,ob = offset(x),offset(y),offset(a),offset(b)
+      ox, oy, oa, ob = offset(x), offset(y), offset(a), offset(b) # return to 0..N-1 range
       return (branch == ((0,0),(0,0))) or (x!=a and y!=b and ox+oy!=oa+ob and ox-oy!=oa-ob)
 
     for branch in product(product(indices, midrc), product(midrc, indices)) if odd_N else combinations(set(product(indices, midrc)) | set(product(midrc, indices)), 2):
-      if legal(branch):
-        S.update(sym(branch))
+      if frozenset(branch) not in B and legal(branch):
+        s = sym(branch)
+        c = len(s)
+        B.update({b:c for b in s})
+        F.add(branch)
     
-    branches, orbits = len(S), dict(sorted(multiset(S.values()).items(), key=lambda o:o[0], reverse=True)) # multiset(S.values()) sorted for printout
+    branches, orbits = len(B), dict(sorted(multiset(B.values()).items(), key=lambda o:o[0], reverse=True))
     fundamental = {k: v//k for k,v in orbits.items()}
     reduced = sum(fundamental.values())
     quotient = branches/reduced if reduced else 0
