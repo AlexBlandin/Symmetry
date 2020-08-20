32e445e6ee795ba3a02d7f238d0f707140aea055
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-08-20 10:57:12 +0100

    Can naturally use symmetry to speed up fundamentals generation

--- gen.py
+++ gen.py
@@ -6,13 +6,15 @@ MIN_N, MAX_N = 1, 20
 for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
   ob_branches, sb_branches = set(), set()
   
-  # Identify the middle rows and columns of the board, based on natural 1..N coordinate-indices
   indices = tuple(range(1,N+1))
-  middle = tuple(indices[(N-1)//2 : N//2+1]) # middle/median indices (single if odd_n else pair)
+  middle = tuple(indices[(N-1)//2 : N//2+1])
   intersection = frozenset(product(middle, middle))
   rows = (product(indices, middle),) if odd_N else (product(indices, middle[:1]), product(indices, middle[1:]))
   cols = (product(middle, indices),) if odd_N else (product(middle[:1], indices), product(middle[1:], indices))
   numrc = len(rows) + len(cols)
+
+  # can trivially half the first row we branch on if we want a speed-up
+  # rows = (product(indices[:N//2+1], middle),) if odd_N else (product(indices[:N//2], middle[:1]), product(indices, middle[1:]))
 
   # Key Functions
   def legal(branch): return ((len(branch) == numrc or
@@ -31,14 +33,22 @@ for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
       ob_branches |= symmetries(branch)
       sb_branches.add(branch)
   
-  # Halved first row we select, so half search space
   dob_branches, dsb_branches = set(), set()
-  rows = (product(indices[:N//2+1], middle),) if odd_N else (product(indices[:N//2], middle[:1]), product(indices, middle[1:]))
-  cols = (product(middle, indices),) if odd_N else (product(middle[:1], indices), product(middle[1:], indices))
-  for branch in map(frozenset, product(*rows, *cols)):
-    if branch not in dob_branches and legal(branch):
-      dob_branches |= symmetries(branch)
-      dsb_branches.add(branch)
+  indices = tuple(range(1,N+1))
+  middle = tuple(indices[(N-1)//2 : N//2+1])
+  for adia in range(middle[0]+1, 2*middle[0]): # we're following diag (try adia?) (middle[0],1) and (1,middle[0]) to the intersection
+    # (x,y) is a square with x,y in 1..N, diag in 2..2N, adia in 1-N..N-1
+    # diag = x+y, fixed x (cols) -> y = diag-x, fixed y (rows) -> x = diag-y
+    # adia = x-y, fixed x (cols) -> y = x-adia, fixed y (rows) -> x = adia+y
+    
+    # diag  adia
+    # 2345  0123 (above 0 are +ve)
+    # 3456  1012 (below 0 are -ve)
+    # 4567  2101 (can use adia for outermost iter?)
+    # 5678  3210 (with diag being quick for inner?)
+    # diag in 2,3,4,5,6,7,8
+    # adia in 3,2,1,0,1,2,3 (-ve left of 0)
+    pass
   
   if (len(ob_branches), len(sb_branches)) != (len(dob_branches), len(dsb_branches)):
     print(f"{N} doesn't match, (ob, sb) are {(len(ob_branches), len(sb_branches))} != {(len(dob_branches), len(dsb_branches))}")
