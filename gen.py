f5c44aa0b5aceb95273b6691b27250861caab85f
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-08-31 16:20:22 +0100

    Issues with not generating with adia below rc1's

--- gen.py
+++ gen.py
@@ -2,7 +2,7 @@
 from itertools import product, combinations, starmap
 
 # Configure
-MIN_N, MAX_N = 1, 20
+MIN_N, MAX_N = 8, 20
 for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
   ob_branches, sb_branches = set(), set()
   
@@ -29,8 +29,9 @@ for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
   
   def include(branch):
     global ob_branches, sb_branches
-    ob_branches |= symmetries(branch)
-    sb_branches.add(branch)
+    s = symmetries(branch)
+    ob_branches |= s
+    sb_branches.add(tuple(sorted(map(sorted,s))[0])) # always get lexographically first branch
 
   if odd_N: # Odd N is solved, so we don't check and have no reason to break
     mid = middle[0] # the intersection of row and column
@@ -45,6 +46,16 @@ for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
       if branch not in ob_branches and legal(branch):
         include(branch)
   
+    # Display & Debugging
+    def board(squares): return ["".join("#" if (x,y) in squares else
+                                        "-" if (x,y) in product(indices,middle) or (x,y) in product(middle,indices) else
+                                        " " for x in indices) for y in indices]
+    def printout(branch, sym=True):
+      print(N, tuple(branch)); bd = [[] for _ in range(N)]
+      for b in symmetries(branch) if sym else [branch]:
+        for i, line in enumerate(board(b)): bd[i].append(line)
+      print("\n".join(" ".join(line) for line in bd));print()
+
     # (x,y) is a square with x,y in 1..N, diag in 2..2N, adia in 1-N..N-1
     # diag = x+y, fixed x (cols) -> y = diag-x, fixed y (rows) -> x = diag-y
     # adia = x-y, fixed x (cols) -> y = x-adia, fixed y (rows) -> x = adia+y
@@ -57,32 +68,55 @@ for N, odd_N in [(N, N%2) for N in range(MIN_N, MAX_N+1)]:
     # diag in 2,3,4,5,6,7,8
     # adia in 3,2,1,0,1,2,3 (-ve left of 0)
 
-    # 23456789 (mod 10 for compactness)
-    # 34567890 
-    # 45678901 
-    # 56789012 
-    # 67890123 
-    # 78901234 
-    # 89012345 
-    # 90123456
+    # 23456789 01234567 (mod 10 for compactness)
+    # 34567890 10123456 (+/- over/under 0 for R)
+    # 45678901 21012345 
+    # 56789012 32101234 
+    # 67890123 43210123 
+    # 78901234 54321012 
+    # 89012345 65432101 
+    # 90123456 76543210 
 
-    #    56    (mod 10, just midrc)
-    #    67    
-    #    78    
-    # 56789012 
-    # 67890123 
-    #    01    
-    #    12    
-    #    23    
+    #    56       34    (mod 10, just midrc)
+    #    67       23    (+-over/under0 on R)
+    #    78       12    
+    # 56789012 32101234 
+    # 67890123 43210123 
+    #    01       21    
+    #    12       32    
+    #    23       43    
 
+    left, right = lambda t: t[0], lambda t: t[1]
     dob_branches, dsb_branches = set(), set()
-    for a in range(1, middle[0]): # first two are following diag spanning (middle[0],1) and (1,middle[0]) inwards until it reaches the intersection (middle[0],middle[0]) (exclusive of intersection), the remaining scanlines carry until they reach the diag constraining the orbits (inclusive of orbit)
-      # (1,middle[0]), (3,middle[1]), (middle[0], 2), (middle[1], 4) # roughly where it starts?
-      diag = [a+middle[0]] # stack of diagonals
-      dlim = [2*N-(a+middle[0])+2] # stack of opposing limits
-      
+    def include(branch):
+      global dob_branches, dsb_branches
+      s = symmetries(branch)
+      dob_branches |= s
+      dsb_branches.add(tuple(sorted(map(sorted,s))[0]))
+    def scanline(diag, coord=[], adia=[]):
+      return {Q for Q in ((middle[0], diag-middle[0]), (middle[1], diag-middle[1]), (diag-middle[1],middle[1])) if sum(Q)==diag and 1<=Q[0]<=N and 1<=Q[1]<=N and Q[0] not in map(left, coord) and Q[1] not in map(right, coord) and Q[0]-Q[1] not in adia}
+    for a in range(1+middle[0], 2*middle[0]):
+      limit = 2*N-a+2 # stack of opposing limits
+      rc1 = (a-middle[0], middle[0]) # stack of coords representing Queens (get diag as sum of tuple)
+      adg = rc1[0]-rc1[1] # antidiagonal
+      for b in range(a+1, limit+1):
+        for rc2 in scanline(b, [rc1], [adg]):
+          for c in range(b+1, limit+1):
+            for rc3 in scanline(c, [rc1, rc2], [adg, rc2[0]-rc2[1]]):
+              if rc2 in intersection or rc3 in intersection:
+                branch = (rc1, rc2, rc3)
+                if legal(frozenset(branch)):
+                  include(branch)
+              else:
+                for d in range(c+1, limit+1):
+                  for rc4 in scanline(c, [rc1, rc2, rc3], [adg, rc2[0]-rc2[1], rc3[0]-rc3[1]]):
+                    branch = (rc1, rc2, rc3, rc4)
+                    if legal(frozenset(branch)):
+                      include(branch)
 
     if (len(ob_branches), len(sb_branches)) != (len(dob_branches), len(dsb_branches)):
+      for branch in sb_branches-dsb_branches:
+        printout(branch,False)
       print(f"{N} doesn't match, (ob, sb) are {(len(ob_branches), len(sb_branches))} != {(len(dob_branches), len(dsb_branches))}")
       break
 else:
